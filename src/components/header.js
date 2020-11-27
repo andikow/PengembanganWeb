5cf99b9fccd371df9f5543ce42aea4ef65ea170e
andikow <katama_andi@yahoo.com>
2020-11-27 20:32:01 +0700

    Update Header dan Readme

--- src/components/header.js
+++ src/components/header.js
@@ -6,36 +6,39 @@ export default class About extends React.Component{
 
     return(
     <>
-        <div className="row d-flex align-content-center flex-wrap bg-dark" style={{height:"50px"}}>
+        <div className="row d-flex align-content-center flex-wrap bg-primary" style={{height:"50px"}}>
           <div className="col-lg-5">
             <button type="button" className="col-lg-12 no-gutters btn btn-secondary" data-toggle="tooltip" data-placement="right" title="Tooltip on right">
               <i className = "fal fa-acorn"></i>Tooltip on right
             </button>
           </div>
           <div className="offset-lg-2"></div>
+
           <div className="col-lg-5 d-flex align-content-center flex-wrap">
-            <button type="button" className="btn btn-sm btn-light mr-2" data-toggle="tooltip" data-placement="right" title="Tooltip on right">
-              <i class="fal fa-shopping-bag fa-fw"></i>Start Selling
-            </button>
-            <button type="button" className="btn btn-sm btn-light mr-2" data-toggle="tooltip" data-placement="right" title="Tooltip on right">
-              <i class="fal fa-shopping-bag fa-fw"></i>Jobs
-            </button>
-            <i class="fal fa-horizontal-rule fa-rotate-90 mr-2"></i>
-            <button type="button" className="btn btn-sm btn-outline-light mr-2" data-toggle="tooltip" data-placement="right" title="Tooltip on right">
-              <i class="fal fa-question-circle fa-fw"></i>Help
-            </button>
-            <button type="button" className="btn btn-sm btn-light mr-2" data-toggle="tooltip" data-placement="right" title="Tooltip on right">
-              <i class="fal fa-user fa-fw"></i>Login
-            </button>
+            <ul class="nav">
+              <li class="nav-item">
+                <a class="nav-link active text-white" href="#">Active</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
           </div>
+
         </div>
         <div className="row d-flex align-content-center flex-wrap bg-light" style={{height:"100px"}}>
           <div className="container">
 
-            <button type="button" className="btn btn-sm btn-info mr-2" data-toggle="tooltip" data-placement="right" title="Tooltip on right">
+            <button type="button" className="btn btn-sm btn-info mr-2">
               <i class="fal fa-shopping-bag fa-fw"></i>Start Selling
             </button>
-            
+
           </div>
         </div>
     </>
