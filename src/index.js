692fd5fe82f782e7b703c98924d949d4ef2e0125
andikow <katama_andi@yahoo.com>
2020-11-29 20:00:25 +0700

    Font dan warna disepakati

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './assets/css/custom.css';
+import './index.css';
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
